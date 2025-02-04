2d63c4450561f9ae7365055c294092ba85b059af
Soham27 <phadkesoham2004@gmail.com>
2025-02-04 14:06:53 +0530

    fixes

--- backend/utils/warmup.py
+++ backend/utils/warmup.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-BACKEND_URI=os.getenv("BACKEND_URI")
+BACKEND_URI=os.getenv("BACKEND_URI","http://127.0.0.1:8000")
 def warm_up_server():
     """Send a request to the server to keep it warm."""
     url = f"{BACKEND_URI}/health"  # Change this to your actual API URL
